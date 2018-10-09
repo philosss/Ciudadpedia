1fbe2f91f880b0f9aab8f0ed7766b7775baa47ab
Filippo Calzavara <filippo.calzavara@mail.polimi.it>
2018-10-09 18:46:55 +0200

    Added app/dataset-reqs routes

--- app/app.js
+++ app/app.js
@@ -5,7 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var datasetRequirements = require('./routes/datasetRequirements.html');
+var applicationRequirements = require('./routes/applicationRequirements.html');
 
 var app = express();
 
@@ -20,22 +21,23 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/datasetRequirements.html', datasetRequirements);
+app.use('/applicationRequirements.html', applicationRequirements);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+	next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
 
 module.exports = app;
