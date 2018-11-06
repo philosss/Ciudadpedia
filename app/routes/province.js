97c37a0c1e17658a6d2cb730202c99e2fa2dbbdc
Filippo Calzavara <filippo.calzavara@mail.polimi.it>
2018-11-06 18:48:04 +0100

    View

--- app/routes/province.js
+++ app/routes/province.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 
 router.get('/:prov_id', function(req, res, next) {
-	res.render('province', { sub: './../', title: 'Provice', prov_id: req.params.prov_id });
+	res.render('province', {
+		sub: './../',
+		title: 'Provice',
+		prov_id: req.params.prov_id,
+		capital: "Madrid"
+	});
 });
 
 module.exports = router;
