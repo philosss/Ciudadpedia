4669e4ff95001e3b136baf2b433da24311999b78
Filippo Calzavara <filippo.calzavara@mail.polimi.it>
2018-12-06 17:50:19 +0100

    Homepage list + parse in prov page

--- app/routes/province.js
+++ app/routes/province.js
@@ -5,9 +5,24 @@ var rp = require('request-promise');
 
 var router = express.Router();
 
-router.get('/:prov_id', function(req, res, next) {
+function toTitleCase(str) {
+	return str.replace(/\w\S*/g, function(txt) {
+		return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+	});
+}
+
+router.get('/:prov_idcode', function(req, res, next) {
+
+	//Doesn't contains special chars
+	prov_idcode_parsed_spaced = req.params.prov_idcode
+		.split('-')
+		.map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+		.join(' ');
+
+	prov_idcode_parsed_undescored = prov_idcode_parsed_spaced.replace(" ", "_")
 
 	// Get the url
+
 	const wikidata_query = `
 	SELECT ?image ?image_banner WHERE {
 		wd:Q2807 wdt:P18 ?image.
@@ -16,7 +31,7 @@ router.get('/:prov_id', function(req, res, next) {
 	const dbpedia_query = `
 	PREFIX dbp: <http://dbpedia.org/resource/>
 	SELECT ?abs WHERE {
-		dbp:Madrid dbo:abstract ?abs.
+		dbp:` + prov_idcode_parsed_undescored + ` dbo:abstract ?abs.
 		FILTER (lang(?abs) = 'en')
 	}`;
 
@@ -34,9 +49,9 @@ router.get('/:prov_id', function(req, res, next) {
 
 					res.render('province', {
 						sub: './../',
-						prov_id: req.params.prov_id,
+						prov_idcode: req.params.prov_idcode,
 						ac_name: "Madrid",
-						prov_name: "Madrid",
+						prov_name: prov_idcode_parsed_spaced,
 						capital_name: "Madrid",
 						title: 'Provice of Madrid',
 						abstract: abs.split(". ").slice(0, 7).join(". "),
